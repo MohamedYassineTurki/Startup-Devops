e2fb85ade42991b2229d679d1c4c143cc0f668d5
yassine <yassineturki200507@gmail.com>
2025-08-14 17:34:26 +0100

    corrected syntax error

--- infra/network/provider.tf
+++ infra/network/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region           = "eu-west-3"
-  profile          = "var.aws_profile"
+  profile          = var.aws_profile
 }
 #this is for version pinning and it's best practice for production environments
 terraform {
