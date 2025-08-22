47b7082a0638296b660c44af0c949c1496487374
yassine <yassineturki200507@gmail.com>
2025-08-22 14:24:31 +0100

    installed the aws controller and made sure that it works

--- infra/eks/iam.tf
+++ infra/eks/iam.tf
@@ -58,7 +58,19 @@ resource "aws_iam_role_policy_attachment" "Node_Group_CNI_Policy_Attachment" {
     policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 resource "aws_iam_role_policy_attachment" "Node_Group_Read_Only_Policy_Attachment" {
-  
     role       = aws_iam_role.BookAdvisor_Node_Group_Role.name
     policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
+#created the iam policy for the aws load balancer controller because there is no managed policy for it
+resource "aws_iam_policy" "AWSLoadBalancerControllerIAMPolicy" {
+  name        = "AWSLoadBalancerControllerIAMPolicy"
+  description = "Permissions for AWS Load Balancer Controller"
+  policy      = file("aws_load_balancer_controller_iam_policy.json") 
+}
+
+resource "aws_iam_role_policy_attachment" "Node_Group_Load_Balancer_Policy_Attachment" {
+  role       = aws_iam_role.BookAdvisor_Node_Group_Role.name
+  policy_arn = aws_iam_policy.AWSLoadBalancerControllerIAMPolicy.arn
 }
